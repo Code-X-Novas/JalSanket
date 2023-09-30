dcce4f27b4b0f6f24bbfcfedf03fb7c2040e5a4b
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2023-09-30 23:30:58 +0530

    commit by sambit

--- app/src/main/java/com/example/jalsanket/MainActivity.java
+++ app/src/main/java/com/example/jalsanket/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.jalsanket;
 
+import static com.example.jalsanket.R.id.clickbutton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickbutton;
+
+        clickbutton = findViewById(R.id.clickbutton);
+
+        clickbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iNext;
+
+                iNext = new Intent(MainActivity.this, Intropage.class);
+                startActivity(iNext);
+            }
+        });
     }
 }
