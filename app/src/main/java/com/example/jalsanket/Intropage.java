51e102beaf03e91bf866206a77998fcba7281ca0
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2023-10-01 17:00:26 +0530

    intent passing from intropage to MainActivity3 by sambit

--- app/src/main/java/com/example/jalsanket/Intropage.java
+++ app/src/main/java/com/example/jalsanket/Intropage.java
@@ -2,7 +2,10 @@ package com.example.jalsanket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intropage extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class Intropage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intropage);
+
+        Button clickbutton;
+
+        clickbutton = findViewById(R.id.clickbutton);
+
+
+        clickbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iNext;
+
+                iNext = new Intent(Intropage.this, MainActivity3.class);
+                startActivity(iNext);
+            }
+        });
     }
 }
