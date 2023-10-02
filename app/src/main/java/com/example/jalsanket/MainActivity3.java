f296dd79ea86102e73d728f71a00123b8eb89511
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2023-10-02 16:11:38 +0530

    commit by sambit(creating bottom navigation)

--- app/src/main/java/com/example/jalsanket/MainActivity3.java
+++ app/src/main/java/com/example/jalsanket/MainActivity3.java
@@ -1,8 +1,13 @@
 package com.example.jalsanket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,8 +20,42 @@ BottomNavigationView botNav;
 
         botNav = findViewById(R.id.botNav);
 
+        botNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+                int id = item.getItemId();
 
-        
+                if (id==R.id.Alerts){
+                 loadfrag(new Alerts(), false);
+
+                } else if (id==R.id.map) {
+                    loadfrag(new Maps(), true);
+
+                } else if (id==R.id.Explore) {
+                    loadfrag(new explore(), false);
+
+                } else if (id==R.id.info) {
+                    loadfrag(new info(), false);
+
+                }else {
+                    loadfrag(new forum(), false);
+
+                }
+                return true;
+            }
+        });
+
+        botNav.setSelectedItemId(R.id.Maps);
+    }
+
+    public void loadfrag(Fragment fragment, boolean flag){
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft= fm.beginTransaction();
+        if (flag)
+            ft.add(R.id.dispscreen, fragment);
+        else
+            ft.replace(R.id.dispscreen,fragment);
+        ft.commit();
     }
 }
