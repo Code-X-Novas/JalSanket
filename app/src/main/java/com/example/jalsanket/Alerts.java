813af1f8eded02c3269053ad945f11b44a0008bf
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2023-10-02 13:08:32 +0530

    commit by sambit

---
+++ app/src/main/java/com/example/jalsanket/Alerts.java
@@ -0,0 +1,33 @@
+package com.example.jalsanket;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Alerts#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Alerts extends Fragment {
+
+
+
+    public Alerts() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alerts, container, false);
+    }
+}
--- app/src/main/java/com/example/jalsanket/MainActivity3.java
+++ app/src/main/java/com/example/jalsanket/MainActivity3.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity3 extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity3 extends AppCompatActivity {
+BottomNavigationView botNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        botNav = findViewById(R.id.botNav);
 
         
     }
